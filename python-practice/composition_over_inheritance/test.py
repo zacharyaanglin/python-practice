42ad38724111b3c3c3207c6e93e6f00fdf3bf148
zacharyaanglin <zach@anglin.com>
2019-03-10 13:48:29 -0400

    first build

---
+++ python-practice/composition_over_inheritance/test.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class PhysicsObject:
+    def __init__(position: List[int]):
+        update_physics()
+        apply_knockback()
+        get_position()
+    
+
+def a_test_function(arg_one: str) -> str:
+    print('I am a test function!')
+    print(arg_one)
