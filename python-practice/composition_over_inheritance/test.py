2d36d1a5edb24878449fc0c3481aad1e01d7b4fb
zacharyaanglin <zach@anglin.com>
2019-03-10 13:50:14 -0400

    Run, it works

--- python-practice/composition_over_inheritance/test.py
+++ python-practice/composition_over_inheritance/test.py
@@ -1,12 +1,15 @@
 from typing import List
 
 class PhysicsObject:
-    def __init__(position: List[int]):
-        update_physics()
-        apply_knockback()
-        get_position()
+    def __init__(self, position: List[int]):
+        pass
     
 
 def a_test_function(arg_one: str) -> str:
     print('I am a test function!')
     print(arg_one)
+
+
+if __name__ == '__main__':
+    a_test_function(__name__)
+    
