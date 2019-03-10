443e64fd1821113d50c553cde9542f611131427a
zacharyaanglin <zach@anglin.com>
2019-03-10 15:37:53 -0400

    Implemented composition over inheritance example

--- python-practice/composition_over_inheritance/test.py
+++ python-practice/composition_over_inheritance/test.py
@@ -1,15 +1,34 @@
+import abc
 from typing import List
 
-class PhysicsObject:
-    def __init__(self, position: List[int]):
+class Name:
+
+    def __init__(self, *args: str):
+        self.names: str = ' '.join(args)
+        self.first = ''
+        if self.names.strip():
+            self.first: str = args[0]
+        try:
+            self.last: str = args[-1]
+        except IndexError:
+            self.last: str = ''
+
+
+class Person(abc.ABC):
+
+    @abc.abstractstaticmethod
+    def talk(words: List[str]) -> None:
         pass
+        
+
+class Anglin(Person):
+
+    def __init__(self, name: Name):
+        self.name = Name
+        self.last = 'Anglin'
     
-
-def a_test_function(arg_one: str) -> str:
-    print('I am a test function!')
-    print(arg_one)
-
-
-if __name__ == '__main__':
-    a_test_function(__name__)
-    
+    @staticmethod
+    def talk(words: List[str]) -> None:
+        print("Hi there!")
+        print(words)
+        print("Goodbye!")
