7f27e8a52f6a59c7c0b3a54b774025f3e3ce9e71
Zachary Anglin <zachary@anglin.com>
2019-04-05 21:34:38 -0400

    Solve the problem.

--- python-practice/project-euler/problem_eighteen/helpers.py
+++ python-practice/project-euler/problem_eighteen/helpers.py
@@ -1,3 +1,32 @@
 """Helper functions for Euler problem eighteen."""
 
-class tree
+from typing import List
+
+import euler_types
+
+
+def initialize_tree(input_list: List[List[str]]) -> List[List[euler_types.EulerTree]]:
+  outer_list = []
+  inner_list = []
+  for row in input_list:
+    for element in row:
+      element = euler_types.EulerTree(value=int(element))
+      inner_list.append(element)
+    outer_list.append(inner_list)
+    inner_list = []
+  return outer_list
+
+
+def load_tree(input_list: List[List[euler_types.EulerTree]]) -> euler_types.EulerTree:
+  """Load an EulerTree from a list of lists of ints."""
+  outer_list = []
+  inner_list = []
+  for idx, row in enumerate(input_list):
+    if len(input_list) > idx + 1:
+      for node_idx, node in enumerate(row):
+        node.left = input_list[idx + 1][node_idx]
+        node.right = input_list[idx + 1][node_idx + 1]
+        inner_list.append(node)
+      outer_list.append(inner_list)
+      inner_list = []
+  return outer_list
--- python-practice/project-euler/problem_eighteen/main.py
+++ python-practice/project-euler/problem_eighteen/main.py
@@ -1,2 +1,12 @@
 """Solve Euler problem eighteen."""
 
+import helpers
+
+if __name__ == '__main__':
+  with open('problem.txt') as file:
+    lines = file.readlines()
+    parsed_lines = [[int(i) for i in line.split()] for line in lines]
+  
+  the_tree = helpers.initialize_tree(parsed_lines)
+  the_loaded_tree = helpers.load_tree(the_tree)
+  print(the_loaded_tree[0][0].compute())
