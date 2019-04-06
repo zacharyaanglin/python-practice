2d91c847527bbb4d59ae7fef6e73698e59787e55
Zachary Anglin <zachary@anglin.com>
2019-04-05 20:42:42 -0400

    Get tree lib working

--- python-practice/project-euler/problem_eighteen/euler_types.py
+++ python-practice/project-euler/problem_eighteen/euler_types.py
@@ -5,6 +5,24 @@ from typing import Any
 
 @dataclass
 class EulerTree:
-	left: Any
-	right: Any
 	value: int
+	left: Any = None
+	right: Any = None
+	computed: bool = False
+	max_val: int = 0
+
+	def compute(self) -> float:
+		"""Compute the correct path for this node."""
+		if not self.left and not self.right:
+			self.computed = True
+			self.max_val = self.value
+			return self.max_val
+		
+		if self.left and not self.left.computed:
+			self.left.compute()
+		
+		if self.right and not self.right.computed:
+			self.right.compute()
+		
+		self.max_val = self.value + max(self.left.max_val, self.right.max_val)
+		return self.max_val
